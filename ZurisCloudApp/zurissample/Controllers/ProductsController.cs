a14739116b782cf81d7a7ffb84bbe43c57a47aa5
haribabuGitHub <haribabuarigela13@gmail.com>
2025-11-20 12:24:27 +0530

    Add product listing functionality and model
    Updated ProductsController to return a static list of products.
    Created a new Product model with properties for Id, Name, and Price.
    Modified Index.cshtml to display products dynamically using a loop.

--- ZurisCloudApp/zurissample/Controllers/ProductsController.cs
+++ ZurisCloudApp/zurissample/Controllers/ProductsController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using zurissample.Models;
 
 namespace zurissample.Controllers
 {
@@ -15,7 +15,15 @@ namespace zurissample.Controllers
         // GET: /Products
         public IActionResult Index()
         {
-            return View();
+            // Return a static list of products as the model for the view
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Product 1", Price = 9.99m },
+                new Product { Id = 2, Name = "Product 2", Price = 19.99m },
+                new Product { Id = 3, Name = "Product 3", Price = 29.99m }
+            };
+
+            return View(products);
         }
 
         // GET: /Products/Details/5
